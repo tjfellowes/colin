dc2ac788a50dde1c4010b890f171988ac7070a52
tjfellowes <fellowes@student.unimelb.edu.au>
2020-02-27 14:21:23 +1100

    test

--- src/models/user.rb
+++ src/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  validates_presence_of :username
+  validates_presence_of :password_hash
+
   # users.password_hash in the database is a :string
   include BCrypt
 
